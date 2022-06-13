60e89877537839efdddc2729461eb3e75ac0d950
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-06-13 18:23:03 -0500

    se agregaron los estilos para que funcionen con webpack.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+//import '@styles/vars.styl';
+
+(async function App() {
+  //const main = null || document.querySelector(".container");
+  //main.innerHTML = await Template();
+})();
