c527e169cf257e8f56da0eb69ff4acc32dcc4e88
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-06-13 19:28:28 -0500

    se agregaron los estilos al html de tareas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 //import '@styles/vars.styl';
 
-(async function App() {
+//agregar un balor tipo yabe varor en el localStarego
+localStorage.setItem("nombre","John");
+
+//recoporar un elemento del local estarege con su yabe.
+console.log(localStorage.getItem("nombre"));
+
+//eleminar elemento del localStorage;
+localStorage.removeItem("nombre");
+
+
+
+//(async function App() {
   //const main = null || document.querySelector(".container");
   //main.innerHTML = await Template();
-})();
+//})();
